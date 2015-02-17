26eedb1799ea008bef61bd5ddbce44c3d9f2e2bb
Siddarth Sivakumar <siddarth.sivakumar@gmail.com>
2015-02-16 21:55:21 -0500

    Adding relationships to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,13 @@
 class Student < ActiveRecord::Base
+
+	#relationships
+	belongs_to :household
+	belongs_to :school
+	has_many :rosterspots
+	has_many :registrations
+	has_many :teams, :through => :rosterspots
+	has_many :guardians, :through => :household
+
+
+
 end
