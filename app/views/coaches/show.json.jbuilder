0f20a743d570e53189c2ebe3127bfc4440077207
Roei <rmc@andrew.cmu.edu>
2015-02-16 21:27:25 -0500

    generated scaffold for coach

---
+++ app/views/coaches/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @coach, :id, :user_id, :team_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ProjectChuck2015::Application.routes.draw do
+  resources :coaches
+
   resources :tournaments
 
   resources :brackets
