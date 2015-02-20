f33b162e42ec5f99bbaee3b76234ba059ce9ab85
Pranita Ramakrishnan <pranitar@andrew.cmu.edu>
2015-02-19 21:47:14 -0500

    Added relationship validations for the coach model

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,2 +1,4 @@
 class Coach < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :team
 end
--- test/models/coach_test.rb
+++ test/models/coach_test.rb
@@ -4,4 +4,6 @@ class CoachTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  should belong_to (:user)
+  should belong_to (:team)
 end
