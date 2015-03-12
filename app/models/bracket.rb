2e5aa4b3266915a0c384ee8b43cd15ccf62e2127
Roei <rmc@andrew.cmu.edu>
2015-03-12 12:42:24 -0400

    this is an error on my part for not committing thoughout, but added most of scopes, validations and custom functions to bracket model. still missing tournament id verification and one or two clarifications about a few of the scopes

--- app/models/bracket.rb
+++ app/models/bracket.rb
@@ -1,9 +1,59 @@
 class Bracket < ActiveRecord::Base
 
-  #Relationship Validations
+  #Relationships 
   has_many :teams
   belongs_to :tournament
   has_many :registrations
   has_many :students, through: :registrations
+
+  #Validations
+  validates_presence_of :gender
+
+  validates_numericality_of :max_students
+  validates_numericality_of :min_age
+  validates_numericality_of :max_age 
+
+  validate :minlessmax
+  validate :valid_tournament_id
+
+  #Scopes
+  #by gender 
+  scope :male, -> { where ("gender = ?"), 'M'}
+  scope :female, -> { where ("gender = ?"), 'F'}
+
+  #by age group
+
+  #is full
+  scope :full, -> { where("max_students = ? "), num_registered }
+  scope :not_full, -> { where("max_students > ?"), num_registered }
+
+
+  #Methods
+  def get_teams
+  	return self.teams
+  end
+
+
+
+  private 
+  #Custom validations
+
+  def minlessmax
+  	return self.min_age < self.max_age 
+  end
+
+  #tournament id must exist in the system
+  #AND end_date of that tournament is null 
+  #i.e. tounament is active
+  def valid_tournament_id
+  	all_tournaments = Tournament.to_a.map{|u| u.id}
+
+  	self.tournament.end_date 
+  end
+
+  #HELPER FUNCTIONS
+  def num_registered
+	return self.registrations.to_a.length
+  end 
 
 end
