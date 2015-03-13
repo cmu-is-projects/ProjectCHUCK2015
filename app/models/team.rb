1645745d3f0b2ebfa196764d143e4bb3528c04fd
Roei <rmc@andrew.cmu.edu>
2015-03-13 16:20:42 -0400

    added all non custom validations to team model

--- app/models/team.rb
+++ app/models/team.rb
@@ -8,4 +8,14 @@ class Team < ActiveRecord::Base
     has_many :students, through: :roster_spots
     has_many :games, through: :team_games
     
+    #validations
+    validates :name, presence: true, uniqueness: true
+
+    validates_numericality_of :num_wins, :num_losses, :max_students
+
+    #custom validation: bracket_id exists in the system
+    #similar to a validation written in bracket. If that one works, the same
+    #format will be used here.
+
+
 end
