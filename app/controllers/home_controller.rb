f1a54ef2f473c61b19daebf73a4c157aae6095a2
Pranita Ramakrishnan <pranitar@andrew.cmu.edu>
2015-03-21 10:06:57 -0400

    Added home and sessions views and controllers and fixed issues associated with bootstrap integration. Added partials for admin vs general views

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,22 @@
+class HomeController < ApplicationController
+  def index
+  	# if logged_in?
+  	# 	if current_user.role? :admin
+  	#   		@upcoming_camps = Camp.upcoming.active.chronological.paginate(:page => params[:page]).per_page(10)
+  	#   		@active_instructors = Instructor.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+  	#   		@students = Student.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+  	#   		@families = Family.active.alphabetical.paginate(:page => params[:page]).per_page(10)
+  	#   	else
+  	#   		# get my camps
+  	#   		@upcoming_camps = current_user.instructor.camps.upcoming.active.chronological.paginate(:page => params[:page]).per_page(10)
+  	#   	end
+   #  end
+  end
+
+  def about
+  end
+
+  def contact
+  end
+  
+end
