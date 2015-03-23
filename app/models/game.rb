df7a323e010c3d0a20f9c606b138181b97bd3206
Siddarth Sivakumar <siddarth.sivakumar@gmail.com>
2015-03-23 19:21:49 -0400

    Edited game model syntax

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,4 +5,15 @@ class Game < ActiveRecord::Base
   has_many :team_games
   has_many :teams, through: :team_games
   
+  #Validations
+  validate :location_is_active_in_system
+
+private
+  def location_is_active_in_system
+    all_active_locations = Location.active.to_a.map{|u| u.id}
+    unless all_active_locations.include?(self.location_id)
+      errors.add(:location_id, "is not an active location in the system")
+    end
+  end
+  
 end
