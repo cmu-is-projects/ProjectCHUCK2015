648ed9870620f1f11fdd9a07b965b7412464d336
Siddarth Sivakumar <siddarth.sivakumar@gmail.com>
2015-04-02 01:29:33 -0400

    Fixed bug with multiple fields nesting inside each other, inclusion of grade, datepicker

--- app/models/student.rb
+++ app/models/student.rb
@@ -20,6 +20,7 @@ class Student < ActiveRecord::Base
   validates_format_of :email, :with => /\A[\w]([^@\s,;]+)@(([a-z0-9.-]+\.)+(com|edu|org|net|gov|mil|biz|info))\z/i, :message => "is not a valid format", :allow_blank => true
   validates_format_of :cell_phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only", :allow_blank => true
   validates_format_of :emergency_contact_phone, with: /\A(\d{10}|\(?\d{3}\)?[-. ]\d{3}[-.]\d{4})\z/, message: "should be 10 digits (area code needed) and delimited with dashes only", :allow_blank => false
+  validates_numericality_of :grade
   GRADESWITHK_ARRAY = [["K", 0]] + (1..12).to_a
   GRADES_ARRAY = (0..12).to_a
   GENDER_ARRAY = ["M","F"]
