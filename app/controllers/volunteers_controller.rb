4334ca89983629e439759b6ed640ea776f7570b7
Allie Wilson <aswilson@andrew.cmu.edu>
2015-04-04 15:26:24 -0400

    fixed issue with heroku display and page loading error. Allowing volunteer form to be accessed without requiring login

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -1,6 +1,6 @@
 class VolunteersController < ApplicationController
   before_action :set_volunteer, only: [:show, :edit, :update, :destroy]
-  before_action :check_login
+  before_action :check_login, :except => [:new, :show]
   authorize_resource
 
   # GET /volunteers
