f0e53d07fc99e062ac1c8cc2034817494ed6f077
Siddarth Sivakumar <siddarth.sivakumar@gmail.com>
2015-04-09 22:17:44 -0400

    Fixed the redirect once logged in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
       user = User.find_by_username(params[:username])
       if user && User.authenticate(params[:username], params[:password])
         session[:user_id] = user.id
-        redirect_to '/home/index2', notice: "You are logged into the Project Chuck system"
+        redirect_to '/home', notice: "You are logged into the Project Chuck system"
       else
         flash.now.alert = "Username or password is invalid"
         render "new"
