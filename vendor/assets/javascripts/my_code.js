b87cbaf26fd47f24d5fc9f885e5cc70b20505561
Allie Wilson <aswilson@andrew.cmu.edu>
2015-04-16 18:53:25 -0400

    copying .js and .css to vendor

---
+++ vendor/assets/javascripts/my_code.js
@@ -0,0 +1,19 @@
+// $(document).ready(function () {
+//   $('.sigPad').signaturePad();
+// });
+// $(function() {
+//     $( "#accordion" ).accordion({
+//       heightStyle: "content"
+//     });
+//   });
+
+window.onload = function(){
+    $( "#accordion" ).accordion({
+      heightStyle: "content"
+    });
+    $('#accordion button').click(function(e) {
+        e.preventDefault();
+        var delta = ($(this).is('.next') ? 1 : -1);
+        $('#accordion').accordion('option', 'active', ( $('#accordion').accordion('option','active') + delta  ));
+    });
+};
