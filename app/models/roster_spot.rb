ca304342e560c9b3a98bf9cc1e5332828089e52a
Pranita Ramakrishnan <pranitar@andrew.cmu.edu>
2015-04-19 23:26:18 -0400

    Edited the team show pages to include coaches and student rosters as well as edited nav bars for admin panel views

--- app/models/roster_spot.rb
+++ app/models/roster_spot.rb
@@ -10,16 +10,16 @@ class RosterSpot < ActiveRecord::Base
   validates_date :end_date, after: :start_date, allow_blank: true
 
   #Scopes
-  scope :by_position, -> { order('position') 
+  scope :by_position, -> { order('position') }
   scope :active, -> { where('end_date IS NULL') }
 
-private
+  private
   def student_is_active_in_system
     all_active_students = Student.active.to_a.map{|u| u.id}
     unless all_active_students.include?(self.student_id)
       errors.add(:student_id, "is not an active student in the system")
     end
   end
-
-
 end
+
+
