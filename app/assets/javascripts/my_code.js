aa478fa4146e58672571b04af69e691cec028756
Roei <rmc@andrew.cmu.edu>
2015-04-25 14:46:56 -0400

    changed livevalidations to cleaner functions, still dont have loop hitting the newly rendered fields

--- app/assets/javascripts/my_code.js
+++ app/assets/javascripts/my_code.js
@@ -6,100 +6,67 @@
 //       heightStyle: "content"
 //     });
 //   });
-    if (window.location.pathname == '/households/new') {
-window.onload = function(){
+if (window.location.pathname == '/households/new') {
+	window.onload = function(){
 
-        $( "#accordion" ).accordion({
-          heightStyle: "content"
-        });
-        $('#accordion button').click(function(e) {
-            e.preventDefault();
-            var delta = ($(this).is('.next') ? 1 : -1);
-            $('#accordion').accordion('option', 'active', ( $('#accordion').accordion('option','active') + delta  ));
-        });
+		$( "#accordion" ).accordion({
+			heightStyle: "content"
+		});
+		$('#accordion button').click(function(e) {
+			e.preventDefault();
+			var delta = ($(this).is('.next') ? 1 : -1);
+			$('#accordion').accordion('option', 'active', ( $('#accordion').accordion('option','active') + delta  ));
+		});
 
 
     //Livevalidations
-    // var home_phone = new LiveValidation('household_home_phone');
+    //household form
+    
+   	var inputs = $('input')
+    for(var i =0; i< inputs.length;i++){
+    	var curr = inputs[i]
+    	var name = curr.name
+    	var id = curr.id
+    	var conditions = [id.indexOf("household") > -1, curr.type != 'hidden', curr.type != 'radio']
+    	drawValidations(conditions);
 
-    //***HOUSEHOLD FORM**** 
+    }
 
-    //HOUSEHOLD FIELDS
-    var city = new LiveValidation('household_city');
-    city.add( Validate.Presence);
-
-    var zip = new LiveValidation('household_zip');
-	zip.add(  Validate.Format, { pattern: /^\d{5}(?:[-\s]\d{4})?$/, failureMessage: "Invalid Format" }  );
-
-	//GUARDIAN FIELDS
-	//will need to loop through all guardians and students
-
-    var pfname = new LiveValidation('household_guardians_attributes_0_first_name');
-    pfname.add( Validate.Presence);
-
-    var plname = new LiveValidation('household_guardians_attributes_0_last_name');
-    plname.add( Validate.Presence);
-
-    var best_num = new LiveValidation('household_guardians_attributes_0_cell_phone');
-    best_num.add( Validate.Presence);
-    best_num.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
-
-	var pemail = new LiveValidation('household_guardians_attributes_0_email');
-    pemail.add( Validate.Format, {pattern:/[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))/i});
-   
-    //STUDENT FIELDS
-    var sfname = new LiveValidation('household_students_attributes_0_first_name');
-    sfname.add( Validate.Presence);
-
-    var slname = new LiveValidation('household_students_attributes_0_last_name');
-    slname.add( Validate.Presence);
+    function drawValidations(conditions){
+    	function dummy(x){ return x }
+    	if(conditions.every(dummy)){
+    		var lv = new LiveValidation(id);
+    		if(curr.required){
+    			lv.add( Validate.Presence )
+    		}
+    		if(name.indexOf("email") > -1){
+    			lv.add( Validate.Format, {pattern:/[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))/i});
+    		}
+    		if(name.indexOf("phone") > -1){
+				lv.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
+    		}
+    		if(name.indexOf("zip") > -1){
+    			lv.add(  Validate.Format, { pattern: /^\d{5}(?:[-\s]\d{4})?$/, failureMessage: "Invalid Format" }  );
+    		}
+    		if(name.indexOf("agree") > -1){
+    			lv.add( Validate.Acceptance );
+    		}
+    	}
+    }
 
 
-    var student_cell = new LiveValidation('household_students_attributes_0_cell_phone');
-    student_cell.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
+    // $('.add_nested_fields').click(function(){
+    // 	var inputs = $('input')
+    // 	for(var i =0; i< inputs.length;i++){
+    // 		var curr = inputs[i]
+    // 		var name = curr.name
+    // 		var id = curr.id
+    // 		var conditions = [id.indexOf("household") > -1, curr.type != 'hidden', curr.type != 'radio', 
+    // 		id.indexOf("_0_") == -1]
+    // 		drawValidations(conditions);
 
-	var semail = new LiveValidation('household_students_attributes_0_email');
-    semail.add( Validate.Format, {pattern:/[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))/i});
-
-    //emergency contact
-    var ec_name = new LiveValidation('household_students_attributes_0_emergency_contact_name');
-    ec_name.add( Validate.Presence);
-
-    var ec_relation = new LiveValidation('household_students_attributes_0_emergency_contact_relation');
-    ec_relation.add( Validate.Presence);
-
-    var ec_phone = new LiveValidation('household_students_attributes_0_emergency_contact_phone');
-    ec_phone.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
-    ec_phone.add( Validate.Presence);
-
-    //medical/insurance
-    var insurance = new LiveValidation('household_students_attributes_0_insurance_provider');
-    insurance.add( Validate.Presence);
-
-    var policy_no = new LiveValidation('household_students_attributes_0_insurance_policy_no');
-    policy_no.add( Validate.Presence);
-
-    var physician = new LiveValidation('household_students_attributes_0_family_physician');
-    physician.add( Validate.Presence);
-
-    var physician_phone = new LiveValidation('household_students_attributes_0_physician_phone');
-    physician_phone.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
-
-    //Agreements
-    var medconsent = new LiveValidation('household_students_attributes_0_parent_consent_agree', {validMessage: "Thank you"});
-	medconsent.add( Validate.Acceptance );
-
-	var parent_promise = new LiveValidation('household_students_attributes_0_parent_promise_agree', {validMessage: "Thank you"});
-	parent_promise.add( Validate.Acceptance );
-
-	var student_promise = new LiveValidation('household_students_attributes_0_child_promise_agree', {validMessage: "Thank you"});
-	student_promise.add( Validate.Acceptance );
-
-	var release = new LiveValidation('household_students_attributes_0_parent_release_agree', {validMessage: "Thank you"});
-	release.add( Validate.Acceptance );
-   	
-   	var street = new LiveValidation('household_street');
-    street.add( Validate.Presence );
+    // 	}
+    // });
 
     $("#myid_1").redraw();
     
