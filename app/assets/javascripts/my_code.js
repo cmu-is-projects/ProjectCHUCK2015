e36d8f8f8fce2497a8b6f8241fcdbdc7a4d3a423
Roei <rmc@andrew.cmu.edu>
2015-04-28 15:35:49 -0400

    played around with livevalidation function, moved functions into document.ready, but validations still dont appear for newly rendered nested fields

--- app/assets/javascripts/my_code.js
+++ app/assets/javascripts/my_code.js
@@ -19,54 +19,8 @@ if (window.location.pathname == '/households/new') {
 		});
 
 
-    //Livevalidations
-    //household form
-    
-   	var inputs = $('input')
-    for(var i =0; i< inputs.length;i++){
-    	var curr = inputs[i]
-    	var name = curr.name
-    	var id = curr.id
-    	var conditions = [id.indexOf("household") > -1, curr.type != 'hidden', curr.type != 'radio']
-    	drawValidations(conditions);
-
-    }
-
-    function drawValidations(conditions){
-    	function dummy(x){ return x }
-    	if(conditions.every(dummy)){
-    		var lv = new LiveValidation(id);
-    		if(curr.required){
-    			lv.add( Validate.Presence )
-    		}
-    		if(name.indexOf("email") > -1){
-    			lv.add( Validate.Format, {pattern:/[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))/i});
-    		}
-    		if(name.indexOf("phone") > -1){
-				lv.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
-    		}
-    		if(name.indexOf("zip") > -1){
-    			lv.add(  Validate.Format, { pattern: /^\d{5}(?:[-\s]\d{4})?$/, failureMessage: "Invalid Format" }  );
-    		}
-    		if(name.indexOf("agree") > -1){
-    			lv.add( Validate.Acceptance );
-    		}
-    	}
-    }
 
 
-    // $('.add_nested_fields').click(function(){
-    // 	var inputs = $('input')
-    // 	for(var i =0; i< inputs.length;i++){
-    // 		var curr = inputs[i]
-    // 		var name = curr.name
-    // 		var id = curr.id
-    // 		var conditions = [id.indexOf("household") > -1, curr.type != 'hidden', curr.type != 'radio', 
-    // 		id.indexOf("_0_") == -1]
-    // 		drawValidations(conditions);
-
-    // 	}
-    // });
 
     $("#myid_1").redraw();
     
@@ -76,6 +30,35 @@ if (window.location.pathname == '/households/new') {
 
 $(document).ready(function() {
   $('.sigPad').signaturePad({drawOnly:true});
+
+  function drawvals(){
+    var inputs = $('input')
+    for(var i =0; i< inputs.length;i++){
+        var curr = inputs[i]
+        var name = curr.name
+        var id = curr.id
+        if(id.indexOf("household") > -1 && curr.type != 'hidden' && curr.type != 'radio'){
+            var lv = new LiveValidation(id);
+            if(curr.required){
+                lv.add( Validate.Presence )
+            }
+            if(name.indexOf("email") > -1){
+                lv.add( Validate.Format, {pattern:/[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))/i});
+            }
+            if(name.indexOf("phone") > -1){
+                lv.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
+            }
+            if(name.indexOf("zip") > -1){
+                lv.add(  Validate.Format, { pattern: /^\d{5}(?:[-\s]\d{4})?$/, failureMessage: "Invalid Format" }  );
+            }
+            if(name.indexOf("agree") > -1){
+                lv.add( Validate.Acceptance );
+            }
+        }
+    }
+  };
+
+drawvals();
 });
 
 $(document).on('nested:fieldRemoved', function (event) {
