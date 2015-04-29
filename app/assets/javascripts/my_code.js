aea32822d497e3d2dd29d0227662ef8460914489
Roei <rmc@andrew.cmu.edu>
2015-04-29 10:06:48 -0400

    figured out how to get newly rendered nested fields to have live validations

--- app/assets/javascripts/my_code.js
+++ app/assets/javascripts/my_code.js
@@ -61,6 +61,33 @@ $(document).ready(function() {
 drawvals();
 });
 
+$(document).on('nested:fieldAdded', function (event) {
+    var inputs = $('input')
+    for(var i =0; i< inputs.length;i++){
+        var curr = inputs[i]
+        var name = curr.name
+        var id = curr.id
+        if(id.indexOf("household") > -1 && curr.type != 'hidden' && curr.type != 'radio' && id.indexOf("_0_") == -1){
+            var lv = new LiveValidation(id);
+            if(curr.required){
+                lv.add( Validate.Presence )
+            }
+            if(name.indexOf("email") > -1){
+                lv.add( Validate.Format, {pattern:/[\w]([^@\s,;]+)@(([\w-]+\.)+(com|edu|org|net|gov|mil|biz|info))/i});
+            }
+            if(name.indexOf("phone") > -1){
+                lv.add( Validate.Format, { pattern: /^\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/, failureMessage: "Invalid format"});
+            }
+            if(name.indexOf("zip") > -1){
+                lv.add(  Validate.Format, { pattern: /^\d{5}(?:[-\s]\d{4})?$/, failureMessage: "Invalid Format" }  );
+            }
+            if(name.indexOf("agree") > -1){
+                lv.add( Validate.Acceptance );
+            }
+        }
+    }  
+});
+
 $(document).on('nested:fieldRemoved', function (event) {
     $('[required]', event.field).removeAttr('required');
   });
