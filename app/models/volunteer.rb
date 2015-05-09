f3d1fcb8910558a61d7d203762d4b397f940d2bb
Pranita Ramakrishnan <pranitar@andrew.cmu.edu>
2015-05-09 12:17:37 -0400

    Added before saves for phone numbers for volunteers

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -7,6 +7,9 @@ class Volunteer < ActiveRecord::Base
   
   mount_uploader :volunteer_sign, AvatarUploader
 
+  before_save :reformat_cell_phone
+  before_save :reformat_day_phone
+
   #validations
 
   STATES_LIST = [['Alabama', 'AL'],['Alaska', 'AK'],['Arizona', 'AZ'],['Arkansas', 'AR'],['California', 'CA'],['Colorado', 'CO'],['Connecticut', 'CT'],['Delaware', 'DE'],['District of Columbia ', 'DC'],['Florida', 'FL'],['Georgia', 'GA'],['Hawaii', 'HI'],['Idaho', 'ID'],['Illinois', 'IL'],['Indiana', 'IN'],['Iowa', 'IA'],['Kansas', 'KS'],['Kentucky', 'KY'],['Louisiana', 'LA'],['Maine', 'ME'],['Maryland', 'MD'],['Massachusetts', 'MA'],['Michigan', 'MI'],['Minnesota', 'MN'],['Mississippi', 'MS'],['Missouri', 'MO'],['Montana', 'MT'],['Nebraska', 'NE'],['Nevada', 'NV'],['New Hampshire', 'NH'],['New Jersey', 'NJ'],['New Mexico', 'NM'],['New York', 'NY'],['North Carolina','NC'],['North Dakota', 'ND'],['Ohio', 'OH'],['Oklahoma', 'OK'],['Oregon', 'OR'],['Pennsylvania', 'PA'],['Rhode Island', 'RI'],['South Carolina', 'SC'],['South Dakota', 'SD'],['Tennessee', 'TN'],['Texas', 'TX'],['Utah', 'UT'],['Vermont', 'VT'],['Virginia', 'VA'],['Washington', 'WA'],['West Virginia', 'WV'],['Wisconsin ', 'WI'],['Wyoming', 'WY']]
@@ -50,6 +53,11 @@ class Volunteer < ActiveRecord::Base
        cell_phone.gsub!(/[^0-9]/,"") # strip all non-digits
        self.cell_phone = cell_phone       # reset self.phone to new string
      end
+
+     def reformat_day_phone
+       day_phone = self.day_phone.to_s  # change to string in case input as all numbers 
+       day_phone.gsub!(/[^0-9]/,"") # strip all non-digits
+       self.day_phone = day_phone       # reset self.phone to new string
+     end
     
-	
 end
