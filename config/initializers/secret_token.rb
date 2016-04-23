a3167a14f68a380a5caaa71755499d4756d5d455
Shreeyagya Khemka <skhemka@andrew.cmu.edu>
2016-04-23 17:50:32 -0400

    Added tokens and credentials

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectChuck2015::Application.config.secret_key_base = '29b76bd563d3e5b35647304be41704ce8ed9b52bb0b31b5524b3fb28ce968a39cbf70f48dbd1b04619410511f3b3fbe16dcaaf6f2314c7417049aee5244eee7f'
